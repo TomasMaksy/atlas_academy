d258ab8b4325920f33236820235a714f9233f65b
Tom Maksimovic <115001855+TomasMaksy@users.noreply.github.com>
2025-03-15 23:48:22 +0200

    Quiz Finished

--- src/app/(private)/quiz/QuestionStep3.tsx
+++ src/app/(private)/quiz/QuestionStep3.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Image, CheckboxGroup } from "@heroui/react";
+import { Button, Image, CheckboxGroup, Tooltip } from "@heroui/react";
 import { CustomCheckbox } from "./custom-checkbox"; // Import from the new file
 
 import { Pacifico } from "next/font/google";
@@ -36,37 +36,69 @@ export default function QuestionStep({
 }: Props) {
 	const [groupSelected, setGroupSelected] = React.useState<string[]>([]);
 
+	function darkenRGBA(rgba: string, amount: number): string {
+		// Extract the RGBA values from the input
+		const rgbaValues = rgba.match(/\d+(\.\d+)?/g); // Match numbers (including decimals) in rgba
+
+		if (!rgbaValues || rgbaValues.length !== 4) {
+			throw new Error("Invalid RGBA string");
+		}
+
+		// Assign values to RGBA object
+		const r = Number(rgbaValues[0]);
+		const g = Number(rgbaValues[1]);
+		const b = Number(rgbaValues[2]);
+		const a = Number(rgbaValues[3]);
+
+		// Decrease each of the RGB values by the amount, ensuring they don't go below 0
+		const newR = Math.max(r - amount, 0);
+		const newG = Math.max(g - amount, 0);
+		const newB = Math.max(b - amount, 0);
+
+		// Return the new darker rgba color with the same alpha value
+		return `rgba(${newR}, ${newG}, ${newB}, ${a})`;
+	}
 	return (
-		<div className="absolute top-0 left-0 w-full h-full flex flex-col justify-end items-center">
-			{/* Image */}
-			<div className="absolute flex items-center w-full z-0 flex-col bottom-[70px] ">
-				<div className="z-10 ">
-					<Image alt="image" src={imageUrl} width={400} className="z-10 " />
-				</div>
-			</div>
+		<div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between items-center">
+			{/* TITLES */}
 			<div
-				className={`${pacifico.className} font-semibold text-3xl text-wrap whitespace-pre-line text-center z-0 mt-24 text-white/70`}
+				className={`${pacifico.className} font-semibold text-5xl text-wrap flex flex-col whitespace-pre-line tracking-[0.07em] text-center z-0 mt-24 text-white/60`}
 			>
-				<span className="text-7xl text-white/50">{title2} </span>
+				<span className="text-7xl text-white/50">{title} </span>
+				{title2}
 				<br />
-				{title}
+			</div>
+
+			{/* Image */}
+			<div className="absolute bottom-[160px] flex items-center justify-top w-full z-40  flex-col ">
+				<div className="z-40">
+					<Image alt="image" src={imageUrl} width={610} className="" />
+				</div>
 			</div>
 			<main className="w-full h-full flex flex-col justify-end">
 				{/* Lower Input + Gradient */}
 				<div
 					className="flex flex-col justify-end items-center align-bottom z-50 h-[700px]"
 					style={{
-						background: `radial-gradient(ellipse at bottom, ${color} 0%, ${color} 25%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 75%)`,
+						background: `radial-gradient(ellipse 110% 100% at bottom, ${darkenRGBA(
+							color,
+							50
+						)} 0%, ${darkenRGBA(
+							color,
+							50
+						)} 25%, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0) 75%)`,
 					}}
 				>
-					<div className="bottom-0 z-20 gap-4 flex flex-col items-center text-center justify-end mb-8 p-4 h-[500px] md:w-[1000px] sm:w-[350px] ">
+					<div className="bottom-0 z-50 gap-4 flex flex-col items-center text-center justify-end mb-8 p-4 h-[500px] md:w-[1000px] sm:w-[350px] ">
 						{" "}
 						<div className=" flex w-full justify-center items-center font-black leading-tight text-white tracking-tighter sm:text-2xl md:text-3xl z-50 text-center">
-							{question}
+							<Tooltip className="z-50" content="Pick one option!">
+								{question}
+							</Tooltip>
 						</div>
-						<div className="flex gap-2 w-full  items-center justify-center text-center">
+						<div className="flex w-full  items-center justify-center text-center">
 							<CheckboxGroup
-								className="flex flex-wrap justify-center items-center gap-2 w-full text-center "
+								className="flex flex-wrap justify-center items-center gap-2 w-full text-center mb-2"
 								orientation="horizontal"
 								value={groupSelected}
 								onValueChange={(value) => {
--- src/app/(private)/quiz/page.tsx
+++ src/app/(private)/quiz/page.tsx
@@ -10,13 +10,15 @@ import { useRouter } from "next/navigation";
 
 import { FormValues } from "@/app/(private)/types";
 import { AnimatePresence, motion } from "framer-motion";
+import { Progress } from "@heroui/react";
 
 const steps = [
 	{
 		title: "Hi! 👋 ",
-		title2: "I am Tommy!",
-		imageUrl: "/Tommy.png",
+		title2: "My name is Esme!",
+		imageUrl: "/esme_1.png",
 		color: "rgba(63, 175, 168, 1)",
+		color2: "#C9D843",
 		question: "What's your name?",
 		inputText: "Enter your name",
 
@@ -26,7 +28,7 @@ const steps = [
 	{
 		title: "Let's get to know you!",
 		title2: "",
-		imageUrl: "/Irven.png",
+		imageUrl: "/irven_1.png",
 		color: "rgba(201, 216, 67, 1)",
 		question: "How would your friends describe you?",
 		inputText: "Enter your name",
@@ -34,9 +36,9 @@ const steps = [
 		options: ["", "", "", "", "", ""],
 	},
 	{
-		title: "",
+		title: "What gets you going? 🔥",
 		title2: "",
-		imageUrl: "/Guido.png",
+		imageUrl: "/guido_4.png",
 		color: "rgba(108, 91, 123, 1)",
 		question: "What motivates you the most in life?",
 		options: [
@@ -50,9 +52,9 @@ const steps = [
 		field: "motivations" as keyof FormValues,
 	},
 	{
-		title: "",
+		title: "Like to tackle problems? 🔝",
 		title2: "",
-		imageUrl: "/Tommy.png",
+		imageUrl: "/esme_2.png",
 		color: "rgba(63, 175, 168, 1)",
 		question: "How do you typically approach challenges?",
 		options: [
@@ -66,9 +68,9 @@ const steps = [
 		field: "challenges" as keyof FormValues,
 	},
 	{
-		title: "",
+		title: "How'd you lead? 🛣️",
 		title2: "",
-		imageUrl: "/Irven.png",
+		imageUrl: "/irven_4.png",
 		color: "rgba(201, 216, 67, 1)",
 		question: "Which of these best describes your leadership style?",
 		options: [
@@ -82,9 +84,9 @@ const steps = [
 		field: "leadership" as keyof FormValues,
 	},
 	{
-		title: "Do you have any...",
-		title2: "hobbies?",
-		imageUrl: "/Guido.png",
+		title: " You are doing great rockstar🎸",
+		title2: "",
+		imageUrl: "/guido_3.png",
 		color: "rgba(108, 91, 123, 1)",
 		question:
 			"What type of extracurricular activities are you most involved in?",
@@ -100,8 +102,8 @@ const steps = [
 	},
 	{
 		title: "",
-		title2: "",
-		imageUrl: "/Esme.png",
+		title2: "Ideal college impression? 🎓",
+		imageUrl: "/esme_2.png",
 		color: "rgba(63, 175, 168, 1)",
 		question: "How do you want colleges to see you?",
 		options: [
@@ -116,8 +118,8 @@ const steps = [
 	},
 	{
 		title: "",
-		title2: "",
-		imageUrl: "/Guido.png",
+		title2: "What's your writing flavour! ✍️",
+		imageUrl: "/guido_1.png",
 		color: "rgba(108, 91, 123, 1)",
 		question: "What’s your preferred storytelling style for essays?",
 		options: [
@@ -132,9 +134,9 @@ const steps = [
 	},
 	{
 		title: "",
-		title2: "",
-		imageUrl: "/Guido.png",
-		color: "rgba(108, 91, 123, 1)",
+		title2: "What should they remember about you? 🔑",
+		imageUrl: "/milo_3.png",
+		color: "rgba(255, 87, 34, 1)",
 		question: "What personal qualities do you want emphasized in your essays?",
 		options: [
 			"Resilience and perseverance",
@@ -147,9 +149,9 @@ const steps = [
 		field: "personalQualities" as keyof FormValues,
 	},
 	{
-		title: "",
+		title: "🌍 Talk about legacy... ",
 		title2: "",
-		imageUrl: "/Esme.png",
+		imageUrl: "/esme_3.png",
 		color: "rgba(63, 175, 168, 1)",
 		question: "What kind of impact do you hope to make in the future?",
 		options: [
@@ -164,8 +166,8 @@ const steps = [
 	},
 	{
 		title: "",
-		title2: "",
-		imageUrl: "/Guido.png",
+		title2: "Tell me about your secret\n superpowers 🦸‍♂️",
+		imageUrl: "/guido_4.png",
 		color: "rgba(108, 91, 123, 1)",
 		question:
 			"What is your biggest strength in academic or professional settings?",
@@ -180,10 +182,10 @@ const steps = [
 		field: "academicStrenght" as keyof FormValues,
 	},
 	{
-		title: "",
+		title: "How do you kick things off?",
 		title2: "",
-		imageUrl: "/Guido.png",
-		color: "rgba(108, 91, 123, 1)",
+		imageUrl: "/milo_4.png",
+		color: "rgba(255, 87, 34, 1)",
 		question: "How do you prefer to introduce yourself in an essay?",
 		options: [
 			"A personal anecdote that highlights my character",
@@ -196,56 +198,74 @@ const steps = [
 		field: "introduction" as keyof FormValues,
 	},
 	{
-		title: "",
+		title: "🔑⏳",
 		title2: "Identify Key Moments in Your Life",
-		imageUrl: "/Guido.png",
+		imageUrl: "/esme_4.png",
 		color: "rgba(63, 175, 168, 1)",
-		question: `Now, take a moment to think about an event or experience that has played a major role in shaping the person you are today. You may have several, but focus on one that stands out as particularly meaningful. Jot down the key details—without analyzing or reflecting on them just yet. Treat it like a journal entry, simply noting what happened.
-
-		For example:
-- I helped a lost child find their parents at a crowded festival...
-- I built my first computer from scratch...
-- I gave a speech in front of my entire school for the first time...`,
+		question: `Now, take a moment to think about an event or experience that has played a major role in shaping the person you are today.\n You may have several, but focus on one that stands out as particularly meaningful. Jot down the key details—without analyzing or reflecting on them just yet. Treat it like a journal entry, simply noting what happened.`,
 		options: ["", "", "", "", ""],
 		inputText: "",
 		field: "keyMoments" as keyof FormValues,
+		addText: `For example:
+- I helped a lost child find their parents at a crowded festival...
+- I built my first computer from scratch...
+- I gave a speech in front of my entire school for the first time...`,
 	},
 	{
-		title: "",
-		title2: "Focus on a Defining 5-Second Moment",
-		imageUrl: "/Guido.png",
-		color: "rgba(108, 91, 123, 1)",
-		question: `Now, zero in on a pivotal five-second moment within your experience—an instant that marked a shift in your perspective, emotions, or actions. This could be the exact second you made a decision, had a breakthrough realization, or felt a surge of emotion. Describe this moment vividly, capturing the details that made it impactful.
-
-For example:
-"The instant I hit ‘submit’ on my first college application..."
-"The moment my experiment finally worked after months of failure..."
-"The exact second I stepped onto the stage and the spotlight hit me..."
-This brief but powerful moment will serve as the emotional core of your story.
-
-`,
+		title: "Focus on a...",
+		title2: "Defining 5-Second Moment ⏳⚡",
+		imageUrl: "/Milo_3.png",
+		color: "rgba(255, 87, 34, 1)",
+		question: `Now, zero in on a pivotal five-second moment within your experience—an instant that marked a shift in your perspective, emotions, or actions. \nThis could be the exact second you made a decision, had a breakthrough realization, or felt a surge of emotion. Describe this moment vividly, capturing the details that made it impactful.`,
 		options: ["", "", "", "", ""],
 		inputText: "",
 		field: "fiveSec" as keyof FormValues,
+		addText: `For example:
+- The instant I hit ‘submit’ on my first college application..."
+- The moment my experiment finally worked after months of failure..."
+- The exact second I stepped onto the stage and the spotlight hit me..."\n
+This brief but powerful moment will serve as the emotional core of your story.`,
 	},
-	{
-		title: "Dig Deeper: 🕳️ ",
-		title2: "Why Does This Matter?",
-		imageUrl: "/Guido.png",
-		color: "rgba(108, 91, 123, 1)",
-		question: `Now, take a step back and reflect on the moments you’ve captured. What made this experience meaningful? What did it reveal about you? How did it influence your perspective or shape the path you’re on today? Answer the following questions to uncover the deeper significance of your story.
-In what ways did this experience shape your future aspirations?
-Answer:
-Did this experience challenge or change any of your beliefs? If so, how?
-Answer:
-Why was this event so impactful, and what lasting effect did it have on you?
-`,
-		options: ["", "", "", "", ""],
-		inputText: "",
-		field: "digDeeper" as keyof FormValues,
-	},
+	// 	{
+	// 		title: "Dig Deeper: 🕳️ ",
+	// 		title2: "Why Does This Matter?",
+	// 		imageUrl: "/Guido.png",
+	// 		color: "rgba(108, 91, 123, 1)",
+	// 		question: `Now, take a step back and reflect on the moments you’ve captured. What made this experience meaningful? What did it reveal about you? How did it influence your perspective or shape the path you’re on today? Answer the following questions to uncover the deeper significance of your story.
+	// In what ways did this experience shape your future aspirations?
+	// Answer:
+	// Did this experience challenge or change any of your beliefs? If so, how?
+	// Answer:
+	// Why was this event so impactful, and what lasting effect did it have on you?
+	// `,
+	// 		options: ["", "", "", "", ""],
+	// 		inputText: "",
+	// 		field: "digDeeper" as keyof FormValues,
+	// 	},
 ];
 
+function darkenRGBA(rgba: string, amount: number): string {
+	// Extract the RGBA values from the input
+	const rgbaValues = rgba.match(/\d+(\.\d+)?/g); // Match numbers (including decimals) in rgba
+
+	if (!rgbaValues || rgbaValues.length !== 4) {
+		throw new Error("Invalid RGBA string");
+	}
+
+	// Assign values to RGBA object
+	const r = Number(rgbaValues[0]);
+	const g = Number(rgbaValues[1]);
+	const b = Number(rgbaValues[2]);
+	const a = Number(rgbaValues[3]);
+
+	// Decrease each of the RGB values by the amount, ensuring they don't go below 0
+	const newR = Math.max(r - amount, 0);
+	const newG = Math.max(g - amount, 0);
+	const newB = Math.max(b - amount, 0);
+
+	// Return the new darker rgba color with the same alpha value
+	return `rgba(${newR}, ${newG}, ${newB}, ${a})`;
+}
 const Quiz = () => {
 	const [currentStep, setCurrentStep] = useState(0);
 	const router = useRouter();
@@ -308,9 +328,30 @@ const Quiz = () => {
 
 	return (
 		<main
-			className="h-full w-full transition-colors duration-1000 ease-in-out"
-			style={{ backgroundColor: bgColor }}
+			className="h-full w-full transition-colors duration-1000 ease-in-out items-center"
+			style={{
+				background: `${bgColor}`,
+			}}
 		>
+			<div className="items-center flex justify-center z-50">
+				<section className="absolute w-[26vw] opacity-50 -bottom-0 z-50  ">
+					<Progress
+						classNames={{
+							base: "px-0.5 mb-5 ",
+							label: "text-small font-bold text-white",
+							value: "text-small font-bold text-white",
+							indicator: "bg-white/70",
+						}}
+						maxValue={steps.length - 1}
+						minValue={0}
+						showValueLabel={false}
+						size="md"
+						value={currentStep}
+						className="z-50"
+						valueLabel={`${currentStep + 1} of ${steps.length}`}
+					/>
+				</section>
+			</div>
 			{/* <div
 				className={`w-[200vw] transition-all duration-1000 pl-[30%]`}
 				style={{
@@ -325,6 +366,7 @@ const Quiz = () => {
 					className="w-full"
 				/>
 			</div> */}
+
 			<AnimatePresence>
 				<motion.div
 					key={currentStep}
@@ -332,7 +374,7 @@ const Quiz = () => {
 					animate={{ x: 0, opacity: 1 }}
 					exit={{ x: "-100%", opacity: 0.5 }}
 					transition={{ duration: 0.8, ease: "easeInOut" }}
-					className="absolute w-full h-full"
+					className="absolute w-full h-full z-40"
 				>
 					{currentStep === 1 ? (
 						<QuestionStep2
@@ -370,6 +412,7 @@ const Quiz = () => {
 							imageUrl={steps[currentStep].imageUrl}
 							inputText={steps[currentStep].inputText}
 							color={steps[currentStep].color}
+							addText={steps[currentStep].addText}
 							setStep={handleNextStep}
 							value={formValues[steps[currentStep].field]}
 							onChange={(e) => handleInputChange(e, steps[currentStep].field)}
