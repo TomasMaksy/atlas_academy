71945670976992b3b84387c71384e01c9b7921ac
Marcin Pauksztełło <paukstelom@gmail.com>
2025-03-15 19:41:30 +0200

    fix

--- src/app/(private)/call/InteractiveAvatarTextInput.tsx
+++ src/app/(private)/call/InteractiveAvatarTextInput.tsx
@@ -1,71 +1,71 @@
-import { Input, Tooltip, Spinner } from "@heroui/react";
-import { Icon } from "@iconify/react/dist/iconify.js";
+// import { Input, Tooltip, Spinner } from "@heroui/react";
+// import { Icon } from "@iconify/react/dist/iconify.js";
 
 
-interface StreamingAvatarTextInputProps {
-  label: string;
-  placeholder: string;
-  input: string;
-  onSubmit: () => void;
-  setInput: (value: string) => void;
-  endContent?: React.ReactNode;
-  disabled?: boolean;
-  loading?: boolean;
-}
+// interface StreamingAvatarTextInputProps {
+//   label: string;
+//   placeholder: string;
+//   input: string;
+//   onSubmit: () => void;
+//   setInput: (value: string) => void;
+//   endContent?: React.ReactNode;
+//   disabled?: boolean;
+//   loading?: boolean;
+// }
 
-export default function InteractiveAvatarTextInput({
-  label,
-  placeholder,
-  input,
-  onSubmit,
-  setInput,
-  endContent,
-  disabled = false,
-  loading = false,
-}: StreamingAvatarTextInputProps) {
-  function handleSubmit() {
-    if (input.trim() === "") {
-      return;
-    }
-    onSubmit();
-    setInput("");
-  }
+// export default function InteractiveAvatarTextInput({
+//   label,
+//   placeholder,
+//   input,
+//   onSubmit,
+//   setInput,
+//   endContent,
+//   disabled = false,
+//   loading = false,
+// }: StreamingAvatarTextInputProps) {
+//   function handleSubmit() {
+//     if (input.trim() === "") {
+//       return;
+//     }
+//     onSubmit();
+//     setInput("");
+//   }
 
-  return (
-    <Input
-      endContent={
-        <div className="flex flex-row items-center h-full">
-          {endContent}
-          <Tooltip content="Send message">
-            {loading ? (
-              <Spinner
-                className="text-indigo-300 hover:text-indigo-200"
-                size="sm"
-                color="default"
-              />
-            ) : (
-              <button
-                type="submit"
-                className="focus:outline-none"
-                onClick={handleSubmit}
-              >
-                <Icon width={20} icon="solar:4k-bold" />
-              </button>
-            )}
-          </Tooltip>
-        </div>
-      }
-      label={label}
-      placeholder={placeholder}
-      size="sm"
-      value={input}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") {
-          handleSubmit();
-        }
-      }}
-      onValueChange={setInput}
-      isDisabled={disabled}
-    />
-  );
-}
+//   return (
+//     <Input
+//       endContent={
+//         <div className="flex flex-row items-center h-full">
+//           {endContent}
+//           <Tooltip content="Send message">
+//             {loading ? (
+//               <Spinner
+//                 className="text-indigo-300 hover:text-indigo-200"
+//                 size="sm"
+//                 color="default"
+//               />
+//             ) : (
+//               <button
+//                 type="submit"
+//                 className="focus:outline-none"
+//                 onClick={handleSubmit}
+//               >
+//                 <Icon width={20} icon="solar:4k-bold" />
+//               </button>
+//             )}
+//           </Tooltip>
+//         </div>
+//       }
+//       label={label}
+//       placeholder={placeholder}
+//       size="sm"
+//       value={input}
+//       onKeyDown={(e) => {
+//         if (e.key === "Enter") {
+//           handleSubmit();
+//         }
+//       }}
+//       onValueChange={setInput}
+//       isDisabled={disabled}
+//     />
+//   );
+// }
